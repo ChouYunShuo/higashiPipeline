aec032906024af0e9867c23028426337bba8d60e
Yun-Shuo Chou <yunshuoc@genome-dev.compbio.cs.cmu.edu>
2024-06-06 19:59:03 -0400

    add print_hdf5_structure func

--- higashi2cellscope/utils.py
+++ higashi2cellscope/utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 import h5py
+import os
+import pickle
+import pprint
 OFFSET_DTYPE = np.int64
 
 
@@ -97,12 +100,65 @@ def copyDataset(item, dest_group, track_type):
         
 
 def fileType(fname: str):
-    return 'npy' if fname.endswith('npy') else 'pkl' if fname.endswith('pkl') else 'csv' if fname.endswith('csv') else 'na'
+    return 'npy' if fname.endswith('npy') else 'pkl' if fname.endswith('pkl') else 'pkl' if fname.endswith('pickle') else 'csv' if fname.endswith('csv') else 'na'
+
+def merge_temp_h5_files(original_h5_path, temp_folder, process_cnt, res):
+    with h5py.File(original_h5_path, 'a') as original_hdf:
+        res_grp = original_hdf["resolutions"][str(res)]
+        cell_groups = res_grp["cells"]
+        for process_id in range(process_cnt):
+            temp_h5_path = os.path.join(temp_folder, f"temp_cells_{process_id}.h5")
+            with h5py.File(temp_h5_path, 'r') as temp_hdf:
+                for cell_key in temp_hdf.keys():
+                    temp_hdf.copy(cell_key, cell_groups)
+            os.remove(temp_h5_path)  # Remove temporary file after merging
+
+def load_pickle(file_path):
+    with open(file_path, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+def print_hdf5_structure(file_path):
+    def print_attrs(name, obj, depth=0):
+        padding = ' ' * (depth * 4)  # 4 spaces for each level of depth
+        if isinstance(obj, h5py.Group):
+            print(f"{padding}Group: {name}")
+            if name.endswith('/cells'):
+                first_child = list(obj.keys())[0] if obj.keys() else None
+                if first_child:
+                    first_child_path = f"{name}/{first_child}"
+                    first_child_obj = obj[first_child]
+                    print_attrs(first_child_path, first_child_obj, depth + 1)
+            else:
+                for key in obj.keys():
+                    print_attrs(f"{name}/{key}", obj[key], depth + 1)
+        elif isinstance(obj, h5py.Dataset):
+            print(f"{padding}Dataset: {name}")
+            print(f"{padding}    Shape: {obj.shape}")
+            print(f"{padding}    Data type: {obj.dtype}")
+
+    with h5py.File(file_path, 'r') as file:
+        print_attrs('', file['/'])
 
 if __name__ == "__main__":
-    with h5py.File("../../../scHDF5_data/Lee_et_al_001.h5", 'r') as f:
-        dataset = f["resolutions/500000/cells/cell_1/tracks"]
-        print(list(dataset.keys()))
+    print_hdf5_structure('/work/magroup/yunshuoc/scHDF5_data/Lee_et_al_002.h5')
+
+    # check pickle file
+    # file_path = '/work/magroup/yunshuoc/Higashi_Pipeline/Ramani_et_al/label_info.pickle'
+    # #file_path = '/work/magroup/yunshuoc/Higashi_Pipeline/4DN_scHi-C_Kim/label_info.pickle'  # Adjust the path to your pickle file
+    # data = load_pickle(file_path)
+    
+    # # Use pprint to print the data structure in a readable format
+    # for key, value in data.items():
+    #     print(f"Key: {key}, Type: {type(value)}")
+    # print(len(data["cell type"]))
+
+
+    # check h5
+
+    # with h5py.File("../../../scHDF5_data/Lee_et_al_001.h5", 'r') as f:
+    #     dataset = f["resolutions/500000/cells/cell_1/tracks"]
+    #     print(list(dataset.keys()))
         # print(len(f[f'resolutions/100000/bins'].get("chrom")))
         # dataset = f["resolutions/100000/cells/cell_id1/pixels/count"]
         # print(f"Dataset shape: {dataset.shape}")
