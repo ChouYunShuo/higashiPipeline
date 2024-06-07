c77c10148e0507b0e37c8223b4ea6f830c1ba018
Yun-Shuo Chou <yunshuoc@genome-dev.compbio.cs.cmu.edu>
2024-06-06 20:00:08 -0400

    add cli handles

---
+++ higashi2cellscope/cli.py
@@ -0,0 +1,21 @@
+import argparse
+from core import generate_hic_file
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate HiC HDF5 files from Higashi")
+    parser.add_argument('-c', '--config', type=str, required=True, help='Path to the config file')
+    parser.add_argument('-m', '--mode', type=str, required=True, choices=['create', 'append', 'print'], help='Mode for the operation')
+    parser.add_argument('-t', '--types', nargs='+', help='Types of operation for append')
+
+    args = parser.parse_args()
+
+    if args.mode == 'append' and args.types is None:
+        parser.error("The -t/--types option is required when mode is 'append'")
+
+    if args.mode != 'append' and args.types is not None:
+        parser.error("The -t/--types option is only available when mode is 'append'")
+
+    generate_hic_file(args.config, args.mode, args.types)
+
+if __name__ == "__main__":
+    main()
