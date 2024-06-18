b763f046ef6cf0dfa75b0937a81b53d7d1dc2b9b
Yun-Shuo Chou <yunshuoc@genome-dev.compbio.cs.cmu.edu>
2024-06-18 18:04:41 -0400

    parse raw contact maps

--- higashi2cellscope/matrixParser.py
+++ higashi2cellscope/matrixParser.py
@@ -5,7 +5,7 @@ from tqdm import trange
 from utils import create_mask
 
 class MatrixParser:
-    def __init__(self, impute_dir, raw_dir, chrom_list, chrom_offset, cell_id, neighbors, res, cytoband_path, embedding_name="exp1", process_cnt=1):
+    def __init__(self, impute_dir, raw_dir, chrom_list, chrom_offset, cell_id, neighbors, res, cytoband_path, embedding_name="exp1", process_cnt=1, is_raw=False):
         self.impute_dir = impute_dir
         self.raw_dir = raw_dir
         self.chrom_list = chrom_list
@@ -16,6 +16,13 @@ class MatrixParser:
         self.cytoband_path = cytoband_path
         self.embedding_name = embedding_name
         self.process_cnt = process_cnt
+        self.is_raw = is_raw
+    
+    def process_proba(self, proba):
+        proba /= np.sum(proba)
+        proba[proba <= 1e-6] = 0.0
+        proba *= 1e6
+        return proba
 
     def __iter__(self):
         if self.process_cnt == 1:
@@ -25,38 +32,42 @@ class MatrixParser:
 
         for idx in range_iter:
             chrom = self.chrom_list[idx].decode('utf-8')
-            origin_sparse = np.load(os.path.join(self.raw_dir, f"chr{chrom}_sparse_adj.npy"), allow_pickle=True)
+            file_path = os.path.join(self.raw_dir, f"chr{chrom}_sparse_adj.npy")
+            origin_sparse = np.load(file_path, allow_pickle=True)
             size = origin_sparse[0].shape[0]
             mask = 1 - create_mask(self.res, self.cytoband_path, -1, chrom, origin_sparse)
 
-            with h5py.File(os.path.join(self.impute_dir, f"chr{chrom}_{self.embedding_name}_nbr_{self.neighbors}_impute.hdf5"), "r") as hdf:
-                coordinates = hdf['coordinates']
-                xs, ys = coordinates[:, 0], coordinates[:, 1]
+            if self.is_raw:
+                cell_coo_origin = origin_sparse[self.cell_id].tocoo()
+                xs, ys, proba = cell_coo_origin.row, cell_coo_origin.col, cell_coo_origin.data
+            else:
+                with h5py.File(os.path.join(self.impute_dir, f"chr{chrom}_{self.embedding_name}_nbr_{self.neighbors}_impute.hdf5"), "r") as hdf:
+                    coordinates = hdf['coordinates']
+                    xs, ys = coordinates[:, 0], coordinates[:, 1]
+                    proba = np.array(hdf[f"cell_{self.cell_id}"])
+                    # proba = np.maximum(proba - np.min(proba), 0.0)
+                    # proba[proba <= 1e-5] = 0.0
+            proba = self.process_proba(proba)
+            m = np.zeros((size, size))
+            np.add.at(m, (xs, ys), proba)
+            np.add.at(m, (ys, xs), proba)
+            m *= mask
 
-                proba = np.array(hdf[f"cell_{self.cell_id}"])
-                proba = np.maximum(proba - np.min(proba), 0.0)
-                proba[proba <= 1e-5] = 0.0
+            proba = m[xs, ys]
+            proba1 = m[ys, xs]
 
-                m = np.zeros((size, size))
-                np.add.at(m, (xs, ys), proba)
-                np.add.at(m, (ys, xs), proba)
-                m *= mask
+            x = np.concatenate([xs, ys]) + self.chrom_offset[idx]
+            y = np.concatenate([ys, xs]) + self.chrom_offset[idx]
+            proba = np.concatenate([proba, proba1])
 
-                proba = m[xs, ys]
-                proba1 = m[ys, xs]
+            sorter = np.lexsort((y, x))
+            x = x[sorter]
+            y = y[sorter]
+            proba = proba[sorter]
 
-                x = np.concatenate([xs, ys]) + self.chrom_offset[idx]
-                y = np.concatenate([ys, xs]) + self.chrom_offset[idx]
-                proba = np.concatenate([proba, proba1])
-
-                sorter = np.lexsort((y, x))
-                x = x[sorter]
-                y = y[sorter]
-                proba = proba[sorter]
-
-                yield {
-                    "bin1_id": x,
-                    "bin2_id": y,
-                    "count": proba,
-                }
+            yield {
+                "bin1_id": x,
+                "bin2_id": y,
+                "count": proba,
+            }
             
